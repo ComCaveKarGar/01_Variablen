57052c8230e405edfda55b306b0eb3cf9eda1dae
Karsten <Moonjumper@gmx.de>
2023-05-09 10:23:51 +0200

    Script Erweitert Max Mütze

--- script_01.js
+++ script_01.js
@@ -9,9 +9,18 @@
 */
 
 // Konsolen-Ausgaben
-console.log("Hi");
+// console.log("Hi");
 // console.log(firstName);
 
 
 /***** 02 Deklaration + Wertzuweisung I *******/
 
+let firstName; // Deklaration (Definition)
+firstName = "Max"; // Wertzuweisung
+console.log(firstName); // Ausgabe
+
+let familyName;
+familyName = "Mütze";
+console.log(familyName);
+
+console.log(firstName, familyName)
